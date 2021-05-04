2bea0b70b68ac02b0f7a745c1e0515d7d97f9e55
nataliekendrick <kendn003@newschool.edu>
2021-05-04 19:07:24 -0400

    new commit
    fixed shuffle button and css

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
 let canvas = document.querySelector("#canvas");
 
 canvas.addEventListener("click", function (event) {
-  console.log("clicking the canvas");
 
+  console.log("clicking the canvas");
   let art = new Array('images/piano.png','images/blue.png',
-                      'images/blue2.png','images/blue3.png','images/blue4.png',
+                      'images/blue2.png','images/blue3.png',
+                      'images/blue4.png',
                       'images/green.png', 'images/green2.png', 'images/green4.png',
                       'images/doodle1.png','images/doodle2.png','images/doodle3.png',
                       'images/doodle4.png','images/doodle5.png','images/doodle6.png',
@@ -13,38 +13,90 @@ canvas.addEventListener("click", function (event) {
 
   let currentArt = art[Math.floor(Math.random() * art.length)];
   var divsize = 100;
+
   var posx = (Math.random() * window.innerWidth - divsize).toFixed();
   var posy = (Math.random() * window.innerHeight - divsize).toFixed();
 
   imgArray = new Image();
   imgArray.classList.add("imgArray");
   imgArray.src = currentArt;
-
+  
   imgArray.style.left = posx + "px";
   imgArray.style.top = posy + "px";
   imgArray.style.position = "fixed";
   imgArray.style.width = Math.floor((Math.random()*180)+150)+"px";
-
   canvas.appendChild(imgArray);
-})
 
+  // moved this block of code into this function above seems to have fixed everything. 
+  let shuffleButton = document.querySelector(".shuffle");
+  shuffleButton.addEventListener("click", function (event) {
+    console.log("clicking the suffle button");
+  
+    Array.from(document.querySelectorAll('.imgArray')).forEach(function(img) {
+        
+      var posx = (Math.random() * window.innerWidth - divsize).toFixed();
+      var posy = (Math.random() * window.innerHeight - divsize).toFixed();
+    
+      img.style.left = posx + "px"; 
+      img.style.top = posy + "px";
+  
+  })
+
+   });
+})
 let clearButton = document.querySelector(".clear");
 clearButton.addEventListener("click", function (event) {
-
   console.log("clicking the clear button");
   canvas.innerHTML = " ";
-
  });
 
-let shuffleButton = document.querySelector(".shuffle");
-shuffleButton.addEventListener("click", function (event) {
-  console.log("clicking the suffle button");
 
-  Array.from(document.querySelectorAll("imgArray")).forEach(function(image) {
+// let canvas = document.querySelector("#canvas");
 
-  var posx = Math.floor(Math.random()*300)+"px";
-  var posy = Math.floor(Math.random()*300)+"px";
+// canvas.addEventListener("click", function (event) {
+//   console.log("clicking the canvas");
 
-})
+//   let art = new Array('images/piano.png','images/blue.png',
+//                       'images/blue2.png','images/blue3.png','images/blue4.png',
+//                       'images/green.png', 'images/green2.png', 'images/green4.png',
+//                       'images/doodle1.png','images/doodle2.png','images/doodle3.png',
+//                       'images/doodle4.png','images/doodle5.png','images/doodle6.png',
+//                       'images/doodle7.png','images/doodle8.png','images/doodle9.png' ); 
 
- });
+//   let currentArt = art[Math.floor(Math.random() * art.length)];
+//   var divsize = 100;
+//   var posx = (Math.random() * window.innerWidth - divsize).toFixed();
+//   var posy = (Math.random() * window.innerHeight - divsize).toFixed();
+
+//   imgArray = new Image();
+//   imgArray.classList.add("imgArray");
+//   imgArray.src = currentArt;
+
+//   imgArray.style.left = posx + "px";
+//   imgArray.style.top = posy + "px";
+//   imgArray.style.position = "fixed";
+//   imgArray.style.width = Math.floor((Math.random()*180)+150)+"px";
+
+//   canvas.appendChild(imgArray);
+//     let shuffleButton = document.querySelector(".shuffle");
+//   shuffleButton.addEventListener("click", function (event) {
+//     console.log("clicking the suffle button");
+  
+//     Array.from(document.querySelectorAll('.imgArray')).forEach(function(img) {
+        
+//       var posx = (Math.random() * window.innerWidth - divsize).toFixed();
+//       var posy = (Math.random() * window.innerHeight - divsize).toFixed();
+    
+//       img.style.left = posx + "px"; 
+//       img.style.top = posy + "px";
+  
+//   })
+// });
+
+
+// let clearButton = document.querySelector(".clear");
+// clearButton.addEventListener("click", function (event) {
+
+//   console.log("clicking the clear button");
+//   canvas.innerHTML = " ";
+// })
